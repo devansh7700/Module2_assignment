2b7436048b7bbdc7d9f3c9e1e9e3050d179211bd
Devanshgarg <dgarg3@academic.rrc.ca>
2025-09-30 21:38:27 -0500

    edited the package.json file to make sure express server is running

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/health", (req, res) => {
+  res.status(200).send("Server is healthy");
+});
+
 // Importing morgan
 import morgan from "morgan";
 
