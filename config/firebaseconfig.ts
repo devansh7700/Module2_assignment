23b98b51516a614d8898c2823023e74c03582791
Devanshgarg <dgarg3@academic.rrc.ca>
2025-11-12 19:50:05 -0600

    secure Firebase configuration ans removed hardcoded service account import

--- config/firebaseconfig.ts
+++ config/firebaseconfig.ts
@@ -1,5 +1,13 @@
 import admin from "firebase-admin";
-import serviceAccount from "serviceaccountkey.json";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+};
 
 if (!admin.apps.length) {
   admin.initializeApp({
