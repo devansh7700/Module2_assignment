128a35fe0a62375e187fe8dc1ed61f646dd1dd09
Devanshgarg <dgarg3@academic.rrc.ca>
2025-11-12 19:57:51 -0600

    implemented .env to the main application

--- src/app.ts
+++ src/app.ts
@@ -7,12 +7,17 @@ import employeeRoutes from "./api/v1/routes/employeeroutes";
 
 import branchRoutes from "./api/v1/routes/branchroutes";
 
+import dotenv from "dotenv";
+
 const app = express();
 
 app.use(express.json());
 
 // Use morgan for HTTP request logging
 app.use(morgan("combined"));
+
+// Load environment variables BEFORE your internal imports!
+dotenv.config();
 
 app.get("/health", (req, res) => {
   res.status(200).send("Server is healthy");
