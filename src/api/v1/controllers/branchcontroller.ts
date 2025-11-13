5cecf48e4da83f93d9ed24370613508ba99ca022
Devanshgarg <dgarg3@academic.rrc.ca>
2025-11-12 18:58:54 -0600

    made some changes in the previous code to amke sure everything is working

--- src/api/v1/controllers/branchcontroller.ts
+++ src/api/v1/controllers/branchcontroller.ts
@@ -29,7 +29,7 @@ export const createBranch = (req: Request, res: Response<ApiResponse<Branch>>): 
   }
 
   const newBranch = branchService.createBranch({ name, address, phone });
-  res.status(201).json({ message: "Branch created", data: newBranch });
+  res.status(201).json({ success: true, message: "Branch created", data: newBranch });
 };
 
 export const updateBranch = (req: Request, res: Response<ApiResponse<Branch>>): void => {
@@ -41,7 +41,7 @@ export const updateBranch = (req: Request, res: Response<ApiResponse<Branch>>): 
     return;
   }
 
-  res.status(200).json({success: true, message: "Branch updated", data: branch });
+   res.status(200).json({ success: true, message: "Branch updated", data: updatedBranch });
 };
 
 export const deleteBranch = (req: Request, res: Response<ApiResponse<null>>): void => {
