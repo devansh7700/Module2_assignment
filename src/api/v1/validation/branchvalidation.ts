f3e568c0e2e365e4910fdcfd5a27328b03535782
Devanshgarg <dgarg3@academic.rrc.ca>
2025-11-15 19:28:46 -0600

    added Branch Joi schemas with OpenAPI component documentation

--- src/api/v1/validation/branchvalidation.ts
+++ src/api/v1/validation/branchvalidation.ts
@@ -1,5 +1,37 @@
 import Joi from "joi";
 
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     CreateBranch:
+ *       type: object
+ *       required:
+ *         - name
+ *         - address
+ *         - phone
+ *       properties:
+ *         name:
+ *           type: string
+ *           example: "Downtown Branch"
+ *         address:
+ *           type: string
+ *           example: "123 Main Street"
+ *         phone:
+ *           type: string
+ *           example: "+1234567890"
+ *
+ *     UpdateBranch:
+ *       type: object
+ *       properties:
+ *         name:
+ *           type: string
+ *         address:
+ *           type: string
+ *         phone:
+ *           type: string
+ */
+
 export const createBranchSchema = Joi.object({
   name: Joi.string().required(),
   address: Joi.string().required(),
